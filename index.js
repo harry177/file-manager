9121de43d3983a3a0d85788ba0576e2f2caac7a4
Harry177 <harry177@rambler.ru>
2023-06-24 16:56:38 +0200

    feat: add readline interface

---
+++ index.js
@@ -0,0 +1,18 @@
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
+const launchApp = async () => {
+    console.log(process.argv);
+  const rl = readline.createInterface({ input, output });
+  const answer = await rl.question(
+    'What do you think of Node.js? '
+);
+
+console.log(
+    `Thank you for your valuable feedback: ${answer}`
+);
+
+rl.close();
+};
+
+launchApp();
