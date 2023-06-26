a5d6b8633d12e7352820975a00abc1c820bd721e
Harry177 <harry177@rambler.ru>
2023-06-26 17:51:47 +0200

    feat: add redirection to home directory

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import * as readline from "node:readline/promises";
-import { stdin as input, stdout as output } from "node:process";
+import { stdin as input, stdout as output, chdir } from "node:process";
+import { currentDir } from "./subprograms/currentDir.js";
+import os from "node:os";
 
 const launchApp = async () => {
   const username = process.argv[2]
     ? process.argv[2].slice(process.argv[2].indexOf("=") + 1)
     : "Anonymus";
   console.log(`Welcome to the File Manager, ${username}!`);
+  const home = os.homedir();
+  chdir(home);
+  currentDir();
   const rl = readline.createInterface({ input, output });
-  //const answer = await rl.question("What do you think of Node.js? ");
-
-  //console.log(`Thank you for your valuable feedback: ${answer}`);
 
   rl.on("close", () => {
     console.log(`Thank you for using File Manager, ${username}, goodbye!`);
@@ -21,7 +23,6 @@ const launchApp = async () => {
       });
     }
   });
-  //rl.close();
 };
 
 launchApp();
