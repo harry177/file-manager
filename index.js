560c3c5f72eeb5ece932dd03a9fd6eb7599ee290
Harry177 <harry177@rambler.ru>
2023-06-26 16:54:08 +0200

    feat: update exit process from the app

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ import { stdin as input, stdout as output } from "node:process";
 const launchApp = async () => {
   const username = process.argv[2]
     ? process.argv[2].slice(process.argv[2].indexOf("=") + 1)
-    : "";
+    : "Anonymus";
   console.log(`Welcome to the File Manager, ${username}!`);
   const rl = readline.createInterface({ input, output });
-  const answer = await rl.question("What do you think of Node.js? ");
+  //const answer = await rl.question("What do you think of Node.js? ");
 
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+  //console.log(`Thank you for your valuable feedback: ${answer}`);
 
-rl.on('close', () => {
-  console.log(`Thank you for using File Manager, ${username}, goodbye!`)
-})
+  rl.on("close", () => {
+    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+  });
+  rl.on("line", (input) => {
+    if (input === ".exit") {
+      rl.close(() => {
+        console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+      });
+    }
+  });
   //rl.close();
 };
 
