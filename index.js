d7e8bef5ce36a4418165ecee4a5824809273af3d
Harry177 <harry177@rambler.ru>
2023-06-26 04:00:50 +0200

    feat: add welcome and exit messages

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 
 const launchApp = async () => {
-    console.log(process.argv);
+  const username = process.argv[2]
+    ? process.argv[2].slice(process.argv[2].indexOf("=") + 1)
+    : "";
+  console.log(`Welcome to the File Manager, ${username}!`);
   const rl = readline.createInterface({ input, output });
-  const answer = await rl.question(
-    'What do you think of Node.js? '
-);
+  const answer = await rl.question("What do you think of Node.js? ");
 
-console.log(
-    `Thank you for your valuable feedback: ${answer}`
-);
+  console.log(`Thank you for your valuable feedback: ${answer}`);
 
-rl.close();
+rl.on('close', () => {
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`)
+})
+  //rl.close();
 };
 
 launchApp();
